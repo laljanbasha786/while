ba8a25bc25a982b4818e5bf13b556666f752e4f8
laljanbasha786 <laljanbasha05@gmail.com>
2024-11-07 17:10:45 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,55 @@
+1// var a=0
+// while(a<10){
+ // a++
+ //     console.log(a); 
+// }
+
+// let a=1
+// do{
+//   console.log(a)
+//   a++
+  
+  
+// }while(a<=10)
+
+// 2.let num = 1;
+// while(num<=10){
+//   if(num%2==0){
+// console.log(num)
+//   }num++
+// }    
+
+
+// var a=1
+// do{
+// if(a%2===0){
+//   console.log(a)
+
+// }a++
+// }while(a<=10)
+
+
+
+// let num=1;
+// while(num<=10){
+//   if(num%3==0){
+//     console.log(num)
+//   }num++
+// }
+
+// let a=1
+// do{
+//   if(a%3==0){
+    
+//     console.log(a)
+//   }a++
+// }while(a<=10)
+// Input: student's marks
+let marks = parseInt(prompt("Enter the student's marks:"));
+
+// Check if the marks fall in the range for grade D
+if (marks >= 35 && marks <= 45) {
+    console.log("Grade: D");
+} else {
+    console.log("Grade is not D");
+}
